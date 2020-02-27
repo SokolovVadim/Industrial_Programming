6e7a66f436b19ad8c7f84b4c44294b03ef957502
Vadim <varem99@mail.ru>
2020-02-27 14:17:49 +0300

    find-number -Wall, dizz-buzz comments and stdin

--- fizz-buzz/solution.cpp
+++ fizz-buzz/solution.cpp
@@ -1,55 +1,109 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <sstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-const int MAX_SIZE = 2 << 6;
+/*const int MAX_SIZE = 2 << 6;
 const int BUF_SIZE = 2 << 10;
+*/
 
+/*const int open_file (const char * fileName, const int flag);
+void  read_file     (const int fd, char* buff);*/
+void  read_sequence (std::string & str);
 
-const int open_file(const char * fileName, const int flag);
-void  read_file(const int fd, char* buff);
-void  read_sequence(char* buff);
-
-// uint32_t reduce(uint32_t x, uint32_t N) ;
-// int mod(int num, int divisor);
 unsigned int mod3( unsigned int a );
 unsigned int mod5( unsigned int a );
 
 int main(int argc, char** argv)
 {
-	// std::cout << "Enter filename:" << std::endl;
+	// std::cout << "Enter:" << std::endl;
 
 	if(argc != 2)
 	{
 		std::cout << "ERROR!" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+	std::string str;
+	std::getline(std::cin, str);
+	read_sequence(str);
+}
 
-	char* filename = argv[1];
-	std::cout << filename << std::endl;
-	int fd = open_file(filename, O_RDONLY);
+/*! \brief performs mod3 operation 
+ *   via binary operations
+ *  \param a number to be moded   
+ */
 
-	char* buff = new char[BUF_SIZE];
 
-	read_file(fd, buff);
-	read_sequence(buff);
-	/*for(int i(0); i < 10; ++i)
+unsigned int mod3( unsigned int a ) {
+    while (a > 5) {
+        int s = 0; /* accumulator for the sum of the digits */
+        while (a != 0) {
+            s = s + (a & 3);
+            a = a >> 2;
+        }
+        a = s;
+    }
+    /* note, at this point: a < 6 */
+    if (a > 2) a = a - 3;
+    return a;
+}
+
+/*! \brief performs mod5 operation 
+ *   via binary operations
+ *  \param a number to be moded   
+ */
+
+
+unsigned int mod5( unsigned int a ) {
+    while (a > 9) {
+        int s = 0; /* accumulator for the sum of the digits */
+        while (a != 0) {
+            s = s + (a & 7);
+            a = (a >> 3) * 3;
+        }
+        a = s;
+    }
+    /* note, at this point: a < 10 */
+    if (a > 4) a = a - 5;
+    return a;
+}
+
+/*! \brief convert arg from str to int
+ *   and prints fizz buzz
+ *  \param str argument of command line to be parsed      
+ */
+
+
+void  read_sequence(std::string & str)
+{
+	//std::string str(buff);
+	//std::cout << str << std::endl;
+	std::stringstream stream(str);
+	int n(0);
+	while(stream >> n)
 	{
-		std::cout << mod(i, 3) << std::endl;
-	}*/
-	
-	delete[] buff;
-	if(close(fd) < 0)
-	{
-		printf("close has failed!\n");
+		n = abs(n);
+		if(mod3(n) == 0 && mod5(n) == 0){
+     		std::cout << "FizzBuzz\n";
+    	}
+    	else if(mod3(n) == 0){
+      		std::cout << "Fizz\n";
+    	}
+    	else if( mod5(n) == 0){
+      		std::cout << "Buzz\n";
+    	}
+    	else{
+      		std::cout << n << "\n";
+    	}
 	}
 }
 
+/*
 const int open_file(const char * fileName, const int flag)
 {
 	int openFlag = open(fileName, flag);	// read & write
@@ -74,81 +128,5 @@ void read_file(const int fd, char* buff)
 	}
 
 	buff[sizeBuff - 1] = '\0';
-
-	// printf("Read from file!\n");
-	/*printf("buff = %s", buff);
-	std::cout << std::endl;*/
-
-	/*if(close(fd) < 0)
-	{
-		printf("close has failed!\n");
-	}*/
-	
-
-}
-
-/*
-// x % N
-uint32_t reduce(uint32_t x, uint32_t N) {
-  return ((uint64_t) x * (uint64_t) N) >> 32 ;
 }
 */
-
-// num % divisor
-
-/*int mod(int num, int divisor) 
-{ 
-    return (num - divisor * (num / divisor)); 
-} */
-
-unsigned int mod3( unsigned int a ) {
-    while (a > 5) {
-        int s = 0; /* accumulator for the sum of the digits */
-        while (a != 0) {
-            s = s + (a & 3);
-            a = a >> 2;
-        }
-        a = s;
-    }
-    /* note, at this point: a < 6 */
-    if (a > 2) a = a - 3;
-    return a;
-}
-
-unsigned int mod5( unsigned int a ) {
-    while (a > 9) {
-        int s = 0; /* accumulator for the sum of the digits */
-        while (a != 0) {
-            s = s + (a & 7);
-            a = (a >> 3) * 3;
-        }
-        a = s;
-    }
-    /* note, at this point: a < 10 */
-    if (a > 4) a = a - 5;
-    return a;
-}
-
-void  read_sequence(char* buff)
-{
-	std::string str(buff);
-	std::cout << str << std::endl;
-	std::stringstream stream(str);
-	int n(0);
-	while(stream >> n)
-	{
-		n = abs(n);
-		if(mod3(n) == 0 && mod5(n) == 0){
-     		std::cout << "FizzBuzz\n";
-    	}
-    	else if(mod3(n) == 0){
-      		std::cout << "Fizz\n";
-    	}
-    	else if( mod5(n) == 0){
-      		std::cout << "Buzz\n";
-    	}
-    	else{
-      		std::cout << n << "\n";
-    	}
-	}
-}
